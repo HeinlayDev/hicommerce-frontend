4ffc0a7548786ee8830a2e74d075d92b19ed964b
heinlay <heinlaydev@gmail.com>
2021-04-01 09:28:18 +0630

    first commit

---
+++ src/admin/apiAdmin.js
@@ -0,0 +1,207 @@
+import { API } from "../config";
+
+export const createCategory = (userId, token ,category) => {
+    // console.log(name, email, password);
+    return fetch(`${API}/category/create/${userId}`,{
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type":"application/json",
+            Authorization:`Bearer ${token}`
+        },
+        body: JSON.stringify(category)
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}; 
+
+export const createProduct = (userId, token ,product) => {
+    // console.log(name, email, password);
+    return fetch(`${API}/product/create/${userId}`,{
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            Authorization:`Bearer ${token}`
+        },
+        body: product
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}; 
+
+export const signin = user => {
+    // console.log(name, email, password);
+    return fetch(`${API}/signin`,{
+        method: "POST",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => {
+        return response.json()
+    })
+    .catch(err => {
+        console.log(err)
+    })
+} 
+
+export const authenticate = (data, next) => {
+    if(typeof window !== 'undefined'){
+        localStorage.setItem('jwt', JSON.stringify(data))
+        next();
+    }
+}
+
+export const signout = (next) => {
+   if (typeof window !== "undefined") {
+       localStorage.removeItem("jwt");
+       next();
+       return fetch(`${API}/signout`,{
+           method: "GET",
+       })
+       .then(response => {
+           console.log('signout', response)
+       })
+       .catch(err => console.log(err));
+   } 
+};
+
+export const isAuthenticated = () => {
+    if(typeof window == 'undefined'){
+       return false
+    }
+    if(localStorage.getItem('jwt')){
+        return JSON.parse(localStorage.getItem('jwt'))
+    } else {
+        return false;
+    }
+};
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: "GET"
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err =>  console.log(err));
+};
+
+export const listOrders = (userId, token) => {
+    return fetch(`${API}/order/list/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization:`Bearer ${token}`
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err =>  console.log(err));
+};
+
+export const getStatusValues = (userId, token) => {
+    return fetch(`${API}/order/status-values/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization:`Bearer ${token}`
+        }
+    })
+    .then(response => {
+        return response.json();
+    })
+    .catch(err =>  console.log(err));
+};
+
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+    return fetch(`${API}/order/${orderId}/status/${userId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ status, orderId })
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+/**
+ * to perform crud on product
+ * get all products
+ * get a single product
+ * update single product
+ * delete single product
+ */
+
+ export const getProducts = () => {
+    return fetch(`${API}/products?limit=undefined`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const deleteProduct = (productId, userId, token) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const getProduct = productId => {
+    return fetch(`${API}/product/${productId}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const updateProduct = (productId, userId, token, product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+
+
+
